00c3a7f5b4f03994be5ce95b1df54569f1716b51
Sakiskid <tylerdrew.home@gmail.com>
2020-08-12 18:12:46 -0500

    added high scores and localstorage for them... but I want to make it modular so next up is an object array that I can save to localstorage. this way I will be able to loop through my list of high scores

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -22,12 +22,35 @@ var descriptionTextEl = document.getElementById("description-text");
 var menuButtonsEl = document.getElementById("buttons-menu");
 var questionEl = document.getElementById("quiz-question");
 var startButtonEl = document.getElementById("buttons-menu__start");
-var timeRemainingEl = document.getElementById('time-remaining');
+var timeRemainingEl = document.getElementById("time-remaining");
+var newHighScoreEl = document.getElementById("new-high-score");
+var highScoresEl = document.getElementById("high-scores");
+var highScoreButtonEl = document.getElementById("high-score-input-btn");
+var scoreEl = document.getElementById("score"); // High scores
+
+var highScore1 = {
+  Name: "",
+  Score: 0,
+  Date: ""
+};
+var highScore2 = {
+  Name: "",
+  Score: 0,
+  Date: ""
+};
+var highScore3 = {
+  Name: "",
+  Score: 0,
+  Date: ""
+};
+var highScores = [];
 var score = 0;
+var totalScore = 0;
 var questionWorth = 10;
 var timePenalty = 5;
 var allottedTimeAtStart = 90;
-var timeLeft = 0; // ANCHOR Question Handling
+var timeLeft = 0;
+var isGameOver = false; // ANCHOR Question Handling
 
 var questionsNotYetAsked = [];
 var questions = [];
@@ -35,6 +58,7 @@ var currentQuestion = {};
 
 function init() {
   addQuestionsToMainScript();
+  initializeHighScores();
 }
 
 function addQuestionsToMainScript() {
@@ -42,6 +66,27 @@ function addQuestionsToMainScript() {
     questions.push(questionsToAdd[i]);
     console.log("Current questions: ", questions);
     questionsNotYetAsked.push(i);
+  }
+}
+
+function initializeHighScores() {
+  //TODO initialize rest of highscores
+  if (localStorage.getItem("HighScore1")) {
+    highScore1 = JSON.parse(localStorage.getItem("HighScore1"));
+  } else {
+    localStorage.setItem("HighScore1", JSON.stringify(highScore1));
+  }
+
+  if (localStorage.getItem("HighScore2")) {
+    highScore2 = JSON.parse(localStorage.getItem("HighScore2"));
+  } else {
+    localStorage.setItem("HighScore2", JSON.stringify(highScore2));
+  }
+
+  if (localStorage.getItem("HighScore3")) {
+    highScore3 = JSON.parse(localStorage.getItem("HighScore3"));
+  } else {
+    localStorage.setItem("HighScore3", JSON.stringify(highScore3));
   }
 }
 
@@ -61,6 +106,7 @@ function displayNextQuestion() {
 }
 
 function displayAnswers() {
+  // FIXME if index 0 question is the first question, then index 0 quesiton is shown again after answering correctly
   var availableButtons = [0, 1, 2, 3];
   var answer = availableButtons[random(availableButtons.length)];
   availableButtons.splice(availableButtons.indexOf(answer), 1);
@@ -81,9 +127,19 @@ function displayAnswers() {
 } // ANCHOR Game Management Functions
 
 
+function reset() {
+  menuButtonsEl.style.display = "none";
+  answerButtonsWrapperEl.style.display = "flex";
+  isGameOver = false;
+  score = 0;
+  totalScore = 0;
+  displayNextQuestion();
+  startTimer();
+}
+
 function correctAnswer() {
   console.log("Answer Correct!");
-  score += questionWorth;
+  addScore(questionWorth);
   displayNextQuestion();
 }
 
@@ -94,16 +150,61 @@ function wrongAnswer() {
 }
 
 function gameOver() {
-  console.log("Game Over! Final Score: ", score);
+  isGameOver = true;
+  totalScore = score + timeLeft;
   gameOverEl.style.display = "flex";
   quizAreaEl.style.display = "none";
+
+  if (totalScore > highScore3.Score) {
+    newHighScore();
+  } else {
+    showHighScores();
+  }
+}
+
+function newHighScore() {
+  newHighScoreEl.style.display = "flex";
+  highScoresEl.style.display = "none";
+}
+
+function saveHighScore() {
+  var highScoreInputElValue = document.getElementById("high-score-input").value;
+  var d = new Date();
+  var today = d.getMonth() + "/" + d.getDay() + "/" + d.getFullYear();
+  console.log("New high score saved!", totalScore);
+
+  if (totalScore > highScore1.Score) {
+    highScore1.Name = highScoreInputElValue;
+    highScore1.Score = totalScore;
+    highScore1.Date = today;
+    localStorage.setItem("HighScore1", JSON.stringify(highScore1));
+  } else if (totalScore > highScore2.Score) {
+    highScore2.Name = highScoreInputElValue;
+    highScore2.Date = today;
+    highScore2.Score = totalScore;
+    localStorage.setItem("HighScore2", JSON.stringify(highScore2));
+  } else if (totalScore > highScore3.Score) {
+    highScore3.Name = highScoreInputElValue;
+    highScore3.Date = today;
+    highScore3.Score = totalScore;
+    localStorage.setItem("HighScore3", JSON.stringify(highScore3));
+  } else {
+    console.log("Something went wrong, high score shouldn't be displaying: Score: ", totalScore);
+  }
+}
+
+function showHighScores() {
+  newHighScoreEl.style.display = "none";
+  highScoresEl.style.display = "flex"; //TODO for each high score, append it
 }
 
 function startTimer() {
   timeLeft = allottedTimeAtStart;
   var timer = setInterval(function () {
-    timeLeft--;
-    updateTimeLeft();
+    if (!isGameOver) {
+      timeLeft--;
+      updateTimeLeft();
+    }
   }, 1000);
 }
 
@@ -113,6 +214,11 @@ function updateTimeLeft() {
   if (timeLeft <= 0) {
     gameOver();
   }
+}
+
+function addScore(_int) {
+  score += _int;
+  scoreEl.textContent = score.toString();
 } // ANCHOR Utility Functions
 
 
@@ -122,10 +228,10 @@ function random(length) {
 
 
 startButtonEl.addEventListener('click', function (event) {
-  menuButtonsEl.style.display = "none";
-  answerButtonsWrapperEl.style.display = "flex";
-  score = 0;
-  displayNextQuestion();
-  startTimer();
+  reset();
+});
+highScoreButtonEl.addEventListener('click', function (event) {
+  saveHighScore();
+  showHighScores();
 });
 init();
--- script.js
+++ script.js
@@ -22,13 +22,41 @@ var descriptionTextEl = document.getElementById("description-text");
 var menuButtonsEl = document.getElementById("buttons-menu");
 var questionEl = document.getElementById("quiz-question");
 var startButtonEl = document.getElementById("buttons-menu__start");
-var timeRemainingEl = document.getElementById('time-remaining');
+var timeRemainingEl = document.getElementById("time-remaining");
+var newHighScoreEl = document.getElementById("new-high-score");
+var highScoresEl = document.getElementById("high-scores");
+var highScoreButtonEl = document.getElementById("high-score-input-btn");
+var scoreEl = document.getElementById("score");
+
+// High scores
+var highScore1 = {
+    Name: "",
+    Score: 0,
+    Date: ""
+};
+var highScore2 = {
+    Name: "",
+    Score: 0,
+    Date: ""
+};
+var highScore3 = {
+    Name: "",
+    Score: 0,
+    Date: ""
+};
+
+
+var highScores = []
 
 var score = 0;
+var totalScore = 0;
 var questionWorth = 10;
 var timePenalty = 5;
 var allottedTimeAtStart = 90;
 var timeLeft = 0;
+var isGameOver = false;
+
+
 
 // ANCHOR Question Handling
 let questionsNotYetAsked = [];
@@ -37,6 +65,7 @@ let currentQuestion = {};
 
 function init() {
     addQuestionsToMainScript();
+    initializeHighScores();
 }
 
 function addQuestionsToMainScript() {
@@ -44,6 +73,25 @@ function addQuestionsToMainScript() {
         questions.push(questionsToAdd[i]);
         console.log("Current questions: ", questions);
         questionsNotYetAsked.push(i);
+    }
+}
+
+function initializeHighScores() {
+    //TODO initialize rest of highscores
+    if(localStorage.getItem("HighScore1")){
+        highScore1 = JSON.parse(localStorage.getItem("HighScore1"));
+    } else {
+        localStorage.setItem("HighScore1", JSON.stringify(highScore1));
+    }
+    if(localStorage.getItem("HighScore2")){
+        highScore2 = JSON.parse(localStorage.getItem("HighScore2"));
+    } else {
+        localStorage.setItem("HighScore2", JSON.stringify(highScore2));
+    }
+    if(localStorage.getItem("HighScore3")){
+        highScore3 = JSON.parse(localStorage.getItem("HighScore3"));
+    } else {
+        localStorage.setItem("HighScore3", JSON.stringify(highScore3));
     }
 }
 
@@ -63,6 +111,7 @@ function displayNextQuestion() {
 }
 
 function displayAnswers() {
+    // FIXME if index 0 question is the first question, then index 0 quesiton is shown again after answering correctly
     let availableButtons = [0, 1, 2, 3];
 
     let answer = availableButtons[random(availableButtons.length)];
@@ -89,9 +138,19 @@ function displayAnswers() {
 }
 
 // ANCHOR Game Management Functions
+function reset() {
+    menuButtonsEl.style.display = "none";
+    answerButtonsWrapperEl.style.display = "flex";
+    isGameOver = false;
+    score = 0;
+    totalScore = 0;
+    displayNextQuestion();
+    startTimer();
+}
+
 function correctAnswer() {
     console.log("Answer Correct!");
-    score += questionWorth;
+    addScore(questionWorth);
     displayNextQuestion();
 }
 
@@ -102,16 +161,66 @@ function wrongAnswer() {
 }
 
 function gameOver() {
-    console.log("Game Over! Final Score: ", score);
+    isGameOver = true;
+    totalScore = score + timeLeft;
     gameOverEl.style.display = "flex";
     quizAreaEl.style.display = "none";
+
+    if (totalScore > highScore3.Score) {
+        newHighScore();
+    } else {
+        showHighScores();
+    }
+}
+
+function newHighScore() {
+    newHighScoreEl.style.display = "flex";
+    highScoresEl.style.display = "none";
+}
+
+function saveHighScore() {
+    var highScoreInputElValue = document.getElementById("high-score-input").value;
+    var d = new Date();
+    var today = d.getMonth() + "/" + d.getDay() + "/" + d.getFullYear();
+
+    console.log("New high score saved!", totalScore);
+
+    if(totalScore > highScore1.Score){
+        highScore1.Name = highScoreInputElValue;
+        highScore1.Score = totalScore;
+        highScore1.Date = today;
+        localStorage.setItem("HighScore1", JSON.stringify(highScore1));
+    }
+    else if(totalScore > highScore2.Score) {
+        highScore2.Name = highScoreInputElValue;
+        highScore2.Date = today;
+        highScore2.Score = totalScore;
+        localStorage.setItem("HighScore2", JSON.stringify(highScore2));
+    }
+    else if (totalScore > highScore3.Score) {
+        highScore3.Name = highScoreInputElValue;
+        highScore3.Date = today;
+        highScore3.Score = totalScore;
+        localStorage.setItem("HighScore3", JSON.stringify(highScore3));
+    }
+    else { console.log ("Something went wrong, high score shouldn't be displaying: Score: ", totalScore); }
+}
+
+function showHighScores() {
+    newHighScoreEl.style.display = "none";
+    highScoresEl.style.display = "flex";
+
+    
+    //TODO for each high score, append it
 }
 
 function startTimer() {
     timeLeft = allottedTimeAtStart;
     var timer = setInterval(function () {
-        timeLeft--;
-        updateTimeLeft();
+        if(!isGameOver) {
+            timeLeft--;
+            updateTimeLeft();
+        }
     }, 1000);
 }
 
@@ -122,19 +231,24 @@ function updateTimeLeft() {
     }
 }
 
+function addScore (int) {
+    score += int;
+    scoreEl.textContent = score.toString();
+}
+
 // ANCHOR Utility Functions
 function random(length) {
     return Math.floor(Math.random() * length);
 }
 
-
 // ANCHOR Event Listeners
 startButtonEl.addEventListener('click', function (event) {
-    menuButtonsEl.style.display = "none";
-    answerButtonsWrapperEl.style.display = "flex";
-    score = 0;
-    displayNextQuestion();
-    startTimer();
+    reset();
+});
+
+highScoreButtonEl.addEventListener('click', function (event) {
+    saveHighScore();
+    showHighScores();
 });
 
 init();
